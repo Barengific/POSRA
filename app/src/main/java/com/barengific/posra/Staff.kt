5a3acccf83c333f21e1734b7423739e7cacdff9a
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-09 21:52:24 +0100

    Update Staff.kt

--- app/src/main/java/com/barengific/posra/Staff.kt
+++ app/src/main/java/com/barengific/posra/Staff.kt
@@ -8,7 +8,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class Staff (
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "firstName") val firstName: String?,
+    @ColumnInfo(name = "firstName") var firstName: String?,
     @ColumnInfo(name = "lastName") var lastName: String?,
     @ColumnInfo(name = "email") var email: String?,
     @ColumnInfo(name = "phoneNumber") var phoneNumber: String?,
