eff0f6a252e36eef84f8d66846860044699a6cff
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-10 14:40:33 +0100

    Update AddProduct.kt

--- app/src/main/java/com/barengific/posra/AddProduct.kt
+++ app/src/main/java/com/barengific/posra/AddProduct.kt
@@ -3,6 +3,7 @@ package com.barengific.posra
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -16,15 +17,42 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.barengific.posra.MainActivity.Companion.recyclerView
+import com.barengific.posra.databinding.AddproductLayoutBinding
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 
 class AddProduct : AppCompatActivity() {
+    private lateinit var binding: AddproductLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = AddproductLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //db initialise
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("bob".toCharArray())
+        val factory = SupportFactory(passphrase)
+        val room = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-names")
+            .openHelperFactory(factory)
+            .allowMainThreadQueries()
+            .build()
+        val productDAO = room.productDao()
+
+        //recycle view
+        val arr = productDAO.getAll()
+        val adapter = CustomAdapter(arr)
+        recyclerView = findViewById<View>(R.id.rView) as RecyclerView
+        recyclerView.setHasFixedSize(false)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
 
     }
 
