9f911d83623777efce0bca9f93f659f24c7a188f
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-10 14:37:13 +0100

    Create AddProduct.kt

---
+++ app/src/main/java/com/barengific/posra/AddProduct.kt
@@ -0,0 +1,39 @@
+package com.barengific.posra
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+
+class AddProduct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    private fun manageUsers(){
+
+    }
+    private fun serverCustomer(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+}
