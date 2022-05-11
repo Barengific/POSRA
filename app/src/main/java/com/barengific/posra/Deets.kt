c30703c050ae99a313a7776b26c4388e87ccc4e0
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-11 21:11:14 +0100

    Create Deets.kt

---
+++ app/src/main/java/com/barengific/posra/Deets.kt
@@ -0,0 +1,7 @@
+package com.barengific.posra
+
+class Deets {
+    companion object{
+        var arrr: MutableList<Product> = mutableListOf(Product(0, "1", "11", "111", "1111", "11111", "111111"))
+    }
+}
