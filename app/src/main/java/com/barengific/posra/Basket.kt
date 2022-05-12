223fda1d0f8d886e2a4b88161fa2e60d8eaabbfa
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-12 16:41:15 +0100

    Update Basket.kt

--- app/src/main/java/com/barengific/posra/Basket.kt
+++ app/src/main/java/com/barengific/posra/Basket.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Basket(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "name") var name: String?,
     @ColumnInfo(name = "price") var price: String?,
     @ColumnInfo(name = "qty") var qty: String?,
