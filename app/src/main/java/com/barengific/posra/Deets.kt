86f9d66c04223f9b8097490e90a195d0e1d9b70c
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-13 21:15:21 +0100

    Update Deets.kt

--- app/src/main/java/com/barengific/posra/Deets.kt
+++ app/src/main/java/com/barengific/posra/Deets.kt
@@ -3,5 +3,6 @@ package com.barengific.posra
 class Deets {
     companion object{
         var arrr: MutableList<Basket> = mutableListOf(Basket(0, "", "", "", ""))
+        var bc_value = ""
     }
 }
