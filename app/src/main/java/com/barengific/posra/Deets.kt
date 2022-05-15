fafd9ec4412b0a01ff6f347152a43340c14743e9
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-15 16:53:13 +0100

    Update Deets.kt

--- app/src/main/java/com/barengific/posra/Deets.kt
+++ app/src/main/java/com/barengific/posra/Deets.kt
@@ -4,5 +4,15 @@ class Deets {
     companion object{
         var arrr: MutableList<Basket> = mutableListOf(Basket(0, "", "", "", ""))
         var bc_value = ""
+
+        //update product row
+        var upId = ""
+        var upBarcode = ""
+        var upName = ""
+        var upStockQty = ""
+        var upPrice = ""
+        var upCategory = ""
+        var upUnit = ""
+        var upUnitAs = ""
     }
 }
