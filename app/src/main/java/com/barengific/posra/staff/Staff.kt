e22e7f98fbdba788c58b07758bb1845b41b03968
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-15 20:06:51 +0100

    Create Staff.kt

---
+++ app/src/main/java/com/barengific/posra/staff/Staff.kt
@@ -0,0 +1,19 @@
+package com.barengific.posra.staff
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Staff (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "password") var password: String?,
+    @ColumnInfo(name = "firstName") var firstName: String?,
+    @ColumnInfo(name = "lastName") var lastName: String?,
+    @ColumnInfo(name = "email") var email: String?,
+    @ColumnInfo(name = "phoneNumber") var phoneNumber: String?,
+    @ColumnInfo(name = "dateHired") var dateHired: String?,
+    @ColumnInfo(name = "location") var location: String?,
+    @ColumnInfo(name = "jobTitle") var jobTitle: String?
+)
