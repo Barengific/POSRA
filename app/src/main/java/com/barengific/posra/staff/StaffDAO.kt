a992fc1213c90e76f92360b66ae10ff2cd9d77ac
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-15 20:06:54 +0100

    Create StaffDAO.kt

---
+++ app/src/main/java/com/barengific/posra/staff/StaffDAO.kt
@@ -0,0 +1,34 @@
+package com.barengific.posra.staff
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.barengific.posra.staff.Staff
+
+@Dao
+interface StaffDAO {
+    @Query("SELECT * FROM staff")
+    fun getAll(): List<Staff>
+
+    @Query("SELECT * FROM staff WHERE id IN (:staffIds)")
+    fun loadAllByIds(staffIds: IntArray): List<Staff>
+
+    @Query("SELECT * FROM staff WHERE id LIKE :k")
+    fun findById(k: Int): Staff
+
+    @Query("SELECT * FROM staff WHERE firstName LIKE :k")
+    fun findByFirstName(k: String): Staff
+
+    @Query("SELECT * FROM staff WHERE lastName LIKE :k")
+    fun findByLastName(k: String): Staff
+
+    @Query("SELECT * FROM staff WHERE email LIKE :k")
+    fun findByEmail(k: String): Staff
+
+    @Insert
+    fun insertAll(vararg staff: Staff)
+
+    @Delete
+    fun delete(staff: Staff)
+}
