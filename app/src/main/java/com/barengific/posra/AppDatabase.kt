700f5ab340be272c915593244217935a449ce68e
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-15 20:06:01 +0100

    Update AppDatabase.kt

--- app/src/main/java/com/barengific/posra/AppDatabase.kt
+++ app/src/main/java/com/barengific/posra/AppDatabase.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.barengific.posra.product.Product
+import com.barengific.posra.product.ProductDAO
+import com.barengific.posra.staff.Staff
+import com.barengific.posra.staff.StaffDAO
 
 @Database(entities = [Product::class, Staff::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
