a4d86b1d6e8c11b26eb163d76d9339a09770554a
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-15 20:06:26 +0100

    Create ProductDAO.kt

---
+++ app/src/main/java/com/barengific/posra/product/ProductDAO.kt
@@ -0,0 +1,45 @@
+package com.barengific.posra.product
+
+import androidx.room.*
+import com.barengific.posra.product.Product
+
+
+@Dao
+interface ProductDAO {
+    @Query("SELECT * FROM product")
+    fun getAll(): List<Product>
+
+    @Query("SELECT * FROM product WHERE id IN (:productIds)")
+    fun loadAllByIds(productIds: IntArray): List<Product>
+
+    @Query("SELECT * FROM product WHERE id LIKE :k")
+    fun findByID(k: String): List<Product>
+
+    @Query("SELECT * FROM product WHERE barcode LIKE '%' || :k || '%'")
+    fun findByBarcode(k: String): List<Product>
+
+    @Query("SELECT * FROM product WHERE name LIKE '%' || :k || '%'")
+    fun findByName(k: String): List<Product>
+
+    @Query("SELECT * FROM product WHERE price LIKE '%' || :k || '%'")
+    fun findByPrice(k: String): List<Product>
+
+    @Query("SELECT * FROM product WHERE category LIKE '%' || :k || '%'")
+    fun findByCategory(k: String): List<Product>
+
+    @Query("SELECT * FROM product WHERE unit LIKE '%' || :k || '%'")
+    fun findByUnit(k: String): List<Product>
+
+    @Query("UPDATE product SET barcode = :barcode, name = :name, stockQty = :stockQty, " +
+            "price = :price, category = :category, unit = :unit, unit_as = :unitas WHERE id = :i")
+    fun update(i: String, barcode: String, name: String, stockQty: String, price: String, category: String, unit: String, unitas: String )
+
+    @Update
+    fun updateProduct(product: Product?)
+
+    @Insert
+    fun insertAll(vararg products: Product)
+
+    @Delete
+    fun delete(product: Product)
+}
