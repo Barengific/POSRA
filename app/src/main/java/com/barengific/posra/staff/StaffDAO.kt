3371d01017026d05784041f98a984db0c64d774c
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-15 20:45:53 +0100

    Update StaffDAO.kt

--- app/src/main/java/com/barengific/posra/staff/StaffDAO.kt
+++ app/src/main/java/com/barengific/posra/staff/StaffDAO.kt
@@ -15,16 +15,25 @@ interface StaffDAO {
     fun loadAllByIds(staffIds: IntArray): List<Staff>
 
     @Query("SELECT * FROM staff WHERE id LIKE :k")
-    fun findById(k: Int): Staff
+    fun findById(k: Int): List<Staff>
 
     @Query("SELECT * FROM staff WHERE firstName LIKE :k")
-    fun findByFirstName(k: String): Staff
+    fun findByFirstName(k: String): List<Staff>
 
     @Query("SELECT * FROM staff WHERE lastName LIKE :k")
-    fun findByLastName(k: String): Staff
+    fun findByLastName(k: String): List<Staff>
 
     @Query("SELECT * FROM staff WHERE email LIKE :k")
-    fun findByEmail(k: String): Staff
+    fun findByEmail(k: String): List<Staff>
+
+    @Query("SELECT * FROM staff WHERE phoneNumber LIKE :k")
+    fun findByPhone(k: String): List<Staff>
+
+    @Query("SELECT * FROM staff WHERE location LIKE :k")
+    fun findByLocation(k: String): List<Staff>
+
+    @Query("SELECT * FROM staff WHERE jobTitle LIKE :k")
+    fun findByJob(k: String): List<Staff>
 
     @Insert
     fun insertAll(vararg staff: Staff)
