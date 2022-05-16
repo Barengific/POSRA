750ab830c7df6864272fdbe9a0025b8ea3bb4a1b
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-16 16:10:15 +0100

    Update AddProduct.kt

--- app/src/main/java/com/barengific/posra/product/AddProduct.kt
+++ app/src/main/java/com/barengific/posra/product/AddProduct.kt
@@ -1,10 +1,13 @@
 package com.barengific.posra.product
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.*
 import android.widget.ArrayAdapter
 import android.widget.ImageView
@@ -49,7 +52,8 @@ class AddProduct : AppCompatActivity() {
         binding = AddProductActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        supportActionBar?.hide();
+        actionBar?.hide();
 
         bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.selectedItemId = R.id.nav_add
@@ -58,79 +62,106 @@ class AddProduct : AppCompatActivity() {
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
                 false
-            }
-            else if(menuItem.itemId == R.id.nav_add)  {
+            } else if (menuItem.itemId == R.id.nav_add) {
                 true
-            }
-            else if(menuItem.itemId == R.id.nav_view)  {
+            } else if (menuItem.itemId == R.id.nav_view) {
                 val intent = Intent(this, ViewProduct::class.java)
                 startActivity(intent)
                 true
-            }
-            else  {
+            } else {
                 true
             }
         }
 
-            //db initialise
-            val passphrase: ByteArray = SQLiteDatabase.getBytes("bob".toCharArray())
-            val factory = SupportFactory(passphrase)
-            val room =
-                Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-names")
-                    .openHelperFactory(factory)
-                    .allowMainThreadQueries()
-                    .build()
-            val productDAO = room.productDao()
+        //db initialise
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("bob".toCharArray())
+        val factory = SupportFactory(passphrase)
+        val room =
+            Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-names")
+                .openHelperFactory(factory)
+                .allowMainThreadQueries()
+                .build()
+        val productDAO = room.productDao()
 
-            //recycle view
-            val arr = productDAO.getAll()
-            val adapter = CustomAdapter(arr)
-            recyclerView = binding.rvAddProduct
+        //recycle view
+        val arr = productDAO.getAll()
+        val adapter = CustomAdapter(arr)
+        recyclerView = binding.rvAddProduct
+        recyclerView.setHasFixedSize(false)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        binding.tvBarcode.editText?.setText(Deets.bc_value)
+
+        binding.btnSave.setOnClickListener {
+            val aa = Product(
+                0,
+                binding.tvBarcode.editText?.text.toString(),
+                binding.tvName.editText?.text.toString(),
+                binding.tvStockQty.editText?.text.toString(),
+                binding.tvPrice.editText?.text.toString(),
+                binding.ddCate.editText?.text.toString(),
+                binding.ddUnit.editText?.text.toString(),
+                binding.tvUnitAs.editText?.text.toString()
+            )
+            productDAO.insertAll(aa)
+
+            val arrr = productDAO.getAll()
+            val adapter = CustomAdapter(arrr)
             recyclerView.setHasFixedSize(false)
             recyclerView.adapter = adapter
             recyclerView.layoutManager = LinearLayoutManager(this)
 
-            binding.tvBarcode.editText?.setText(Deets.bc_value)
-
-            binding.btnSave.setOnClickListener {
-                val aa = Product(
-                    0,
-                    binding.tvBarcode.editText?.text.toString(),
-                    binding.tvName.editText?.text.toString(),
-                    binding.tvStockQty.editText?.text.toString(),
-                    binding.tvPrice.editText?.text.toString(),
-                    binding.ddCate.editText?.text.toString(),
-                    binding.ddUnit.editText?.text.toString(),
-                    binding.tvUnitAs.editText?.text.toString()
-                )
-                productDAO.insertAll(aa)
-
-                val arrr = productDAO.getAll()
-                val adapter = CustomAdapter(arrr)
-                recyclerView.setHasFixedSize(false)
-                recyclerView.adapter = adapter
-                recyclerView.layoutManager = LinearLayoutManager(this)
-
-                runOnUiThread {
-                    adapter.notifyDataSetChanged()
-                }
-                //TODO check for duplicates
+            runOnUiThread {
+                adapter.notifyDataSetChanged()
             }
-
-            var linesCate = resources.getStringArray(R.array.dd_cate).toList()
-            var adapterDDCate = ArrayAdapter(this, R.layout.dd_layout, linesCate)
-            binding.ddCateFilled.setAdapter(adapterDDCate)
-
-            var linesUnit = resources.getStringArray(R.array.dd_unit).toList()
-            var adapterDDUnit = ArrayAdapter(this, R.layout.dd_layout, linesUnit)
-            binding.ddUnitFilled.setAdapter(adapterDDUnit)
-
-            binding.tvBarcode.setEndIconOnClickListener {
-                val intent = Intent(this, CamActivity::class.java)
-                startActivity(intent)
-            }
-
+            //TODO check for duplicates
         }
+
+        var linesCate = resources.getStringArray(R.array.dd_cate).toList()
+        var adapterDDCate = ArrayAdapter(this, R.layout.dd_layout, linesCate)
+        binding.ddCateFilled.setAdapter(adapterDDCate)
+
+        var linesUnit = resources.getStringArray(R.array.dd_unit).toList()
+        var adapterDDUnit = ArrayAdapter(this, R.layout.dd_layout, linesUnit)
+        binding.ddUnitFilled.setAdapter(adapterDDUnit)
+
+        binding.tvBarcode.setEndIconOnClickListener {
+            val intent = Intent(this, CamActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.imageView.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        takePictureIntent.type = "image/*";
+        takePictureIntent.putExtra("crop", "true");
+        takePictureIntent.putExtra("outputX", 150);
+        takePictureIntent.putExtra("outputY", 150);
+        takePictureIntent.putExtra("aspectX", 1);
+        takePictureIntent.putExtra("aspectY", 1);
+        takePictureIntent.putExtra("scale", true);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageView.setImageBitmap(imageBitmap)
+        }
+    }
 
 
     class CustomAdapter(private val dataSet: List<Product>) :
