22734a7e935b02c4a90041808d96d847fea12de6
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-17 11:43:53 +0100

    Update Deets.kt

--- app/src/main/java/com/barengific/posra/Deets.kt
+++ app/src/main/java/com/barengific/posra/Deets.kt
@@ -1,7 +1,13 @@
 package com.barengific.posra
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import androidx.recyclerview.widget.RecyclerView
+import com.barengific.posra.product.AddProduct
 import com.barengific.posra.product.Product
 import com.barengific.posra.staff.Staff
+import java.io.ByteArrayOutputStream
 
 class Deets {
     companion object{
@@ -9,6 +15,11 @@ class Deets {
         var arrProduct: MutableList<Product> = mutableListOf(Product(0, "", "", "", "","","","",""))
         var arrStaff: MutableList<Staff> = mutableListOf(Staff(0, "", "", "", "","","","",""))
         var bc_value = ""
+
+        var posProduct: Int = 0
+        fun setPosiProduct(pos: Int) {
+            posProduct = pos
+        }
 
         //update product row
         var upIdProduct = ""
@@ -30,5 +41,23 @@ class Deets {
         var upDateHired = ""
         var upLocation = ""
         var upJobTitle = ""
+
+        lateinit var recyclerView: RecyclerView
+
+        fun encodeImage(bm: Bitmap): String? {
+            val baos = ByteArrayOutputStream()
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val b = baos.toByteArray()
+            AddProduct.imageString = Base64.encodeToString(b, Base64.DEFAULT)
+            return Base64.encodeToString(b, Base64.DEFAULT)
+        }
+
+        fun decodeImage(bm: String): Bitmap? {
+            val decodedString: ByteArray = Base64.decode(bm, Base64.DEFAULT)
+            val decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+            AddProduct.imageBitmap = decodedByte
+            return decodedByte
+        }
     }
+
 }
