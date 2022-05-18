99245acd4138b6af6a5840d3a75c400c6a987ea1
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-18 20:39:18 +0100

    Update Deets.kt

--- app/src/main/java/com/barengific/posra/Deets.kt
+++ app/src/main/java/com/barengific/posra/Deets.kt
@@ -11,11 +11,12 @@ import java.io.ByteArrayOutputStream
 
 class Deets {
     companion object{
-        var arrBasket: MutableList<Basket> = mutableListOf(Basket(0, "", "", "", ""))
+        var arrBasket: MutableList<Basket> = mutableListOf(Basket(0, "0", "", "", ""))
         var arrProduct: MutableList<Product> = mutableListOf(Product(0, "", "", "", "","","","",""))
         var arrStaff: MutableList<Staff> = mutableListOf(Staff(0, "", "", "", "","","","",""))
         var bc_value = ""
-        var btnSaveUpdateState = ""
+
+        var btnSaveUpdateState = "SAVE"
 
         var posProduct: Int = 0
         fun setPosiProduct(pos: Int) {
