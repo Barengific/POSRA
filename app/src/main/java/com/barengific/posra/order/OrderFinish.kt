7bf82193fa90893bc3ef3b5df46582b1e896aabc
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-31 22:22:16 +0100

    Create OrderFinish.kt

---
+++ app/src/main/java/com/barengific/posra/order/OrderFinish.kt
@@ -0,0 +1,17 @@
+package com.barengific.posra.order
+
+class OrderFinish {
+    //TODO
+    //view finished order
+    //continue button to start new customer order
+    //order receipt must contain:
+    //store/shop/supermarket details
+    //order/receipt number
+    //date & time of purchase
+    //list of items, items qty, item price, total of item
+    //total order cost
+    //total order qty
+    //terms & conditions of store/shop
+    //
+
+}
