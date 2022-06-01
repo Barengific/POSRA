b041f25dc55f8bcd8ac3a262d103c1693883176c
Barengific <75044077+Barengific@users.noreply.github.com>
2022-06-01 13:33:00 +0100

    Create OrdersDAO.kt

---
+++ app/src/main/java/com/barengific/posra/order/OrdersDAO.kt
@@ -0,0 +1,36 @@
+package com.barengific.posra.order
+
+import androidx.room.*
+import com.barengific.posra.basket.Basket
+
+@Dao
+interface OrdersDAO {
+    @Query("SELECT * FROM orders")
+    fun getAll(): List<Orders>
+
+    @Query("SELECT * FROM orders WHERE id IN (:orderIds)")
+    fun loadAllByIds(orderIds: IntArray): List<Orders>
+
+    @Query("SELECT * FROM orders WHERE id LIKE :k")
+    fun findByID(k: String): List<Orders>
+
+    @Query("SELECT * FROM orders WHERE date LIKE '%' || :k || '%'")
+    fun findByDate(k: String): List<Orders>
+
+    @Query("SELECT * FROM orders WHERE totalPrice LIKE '%' || :k || '%'")
+    fun findByPrice(k: String): List<Orders>
+
+
+    @Query("UPDATE orders SET date = :date, totalPrice = :totalPrice, totalDiscount = :totalDiscount, " +
+            "totalQty = :totalQty, itemList = :itemList WHERE id = :i")
+    fun update(i: String, date: String, totalPrice: String, totalDiscount: String, totalQty: String, itemList: Basket)
+
+    @Update
+    fun updateOrder(orders: Orders?)
+
+    @Insert
+    fun insertAll(vararg orders: Orders)
+
+    @Delete
+    fun delete(orders: Orders)
+}
